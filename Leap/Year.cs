98ef556d5c6dafa60ad76364d7c51c5b986f3724
Monomachus <monomachus85@yahoo.com>
2015-05-07 17:51:32 +0300

    First commit & Leap solved

---
+++ Leap/Year.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leap
+{
+    public class Year
+    {
+        public static bool IsLeap(int yearNumber) 
+        {
+            if (yearNumber % 400 == 0) {
+                return true;
+            }
+
+            if (yearNumber % 100 == 0) {
+                return false;
+            }
+
+            return yearNumber % 4 == 0;
+        }
+    }
+}
