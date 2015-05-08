2c5862c2a9ebe552dfaa73393b4a7a3af858ec3a
Monomachus <monomachus85@yahoo.com>
2015-05-08 18:34:56 +0300

    Started Bob

---
+++ Bob/Bob.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Bob
+{
+
+    public string Hey(string originalMessage)
+    {
+        string message = originalMessage.Trim();
+
+        if (isSentence(message)) return "Whatever.";
+
+        if (isShouting(message)) return "Whoa, chill out!";
+
+        if (isQuestion(message)) return "Sure.";
+
+        return string.Empty;
+    }
+
+    private bool isQuestion(string message)
+    {
+        return message.EndsWith("?");
+    }
+
+    private static bool isShouting(string message)
+    {
+        return message.EndsWith("!");
+    }
+
+    private static bool isSentence(string message)
+    {
+        return message.EndsWith(".");
+    }
+}
